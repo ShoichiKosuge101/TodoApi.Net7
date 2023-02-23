058b9ee70b0ed3e1f0ae979cf57c102a02e6bc92
ShoichiKosuge101 <aesgaaard@gmail.com>
2023-02-23 23:18:07 +0900

    Shared共有クラスライブラリ作成

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Grpc.Net.Client;
+using MagicOnion.Client;
+using TodoApi.Shared;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
